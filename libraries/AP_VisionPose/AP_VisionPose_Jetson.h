96f177f6557baf483b338e56e55d72c71169e8c7
alessandro-benini <alessandro.benini@outlook.com>
2016-06-02 11:49:09 -0600

    Removed previous JSON library. Added JSMN.

--- libraries/AP_VisionPose/AP_VisionPose_Jetson.h
+++ libraries/AP_VisionPose/AP_VisionPose_Jetson.h
@@ -10,6 +10,8 @@
 #include "AP_VisionPose.h"
 #include "AP_VisionPose_Backend.h"
 
+#include "jsmn.h"
+
 class AP_VisionPose_Jetson : public AP_VisionPose_Backend {
 public:
 	AP_VisionPose_Jetson(AP_VisionPose &_vision_pose, AP_VisionPose::VisionPose_State &_state, AP_HAL::UARTDriver *_port);
