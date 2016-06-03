56584f7dbeb446078dbfdec90463394322791e3f
alessandro-benini <alessandro.benini@outlook.com>
2016-06-03 11:36:57 -0600

    Able to detect the string codified using the JSON format

--- libraries/AP_VisionPose/AP_VisionPose_Jetson.cpp
+++ libraries/AP_VisionPose/AP_VisionPose_Jetson.cpp
@@ -15,49 +15,49 @@ AP_VisionPose_Jetson::AP_VisionPose_Jetson(AP_VisionPose &_vision_pose, AP_Visio
 
 bool AP_VisionPose_Jetson::read(void)
 {
-	    uint8_t d;
 	    char c;
 	    int16_t numc;
 	    bool parsed = false;
 
 	    numc = port->available();
-	    hal.console->printf("Number of bytes available on serial: %u\n", numc);
+	    hal.console->printf("Number of bytes available on serial: %u", numc);
 
+	    char msg[200];
+
+	    bool found_header = false;
+	    uint8_t j = 0;
+
+	    // Need to check overflow
 	    for (int16_t i = 0; i < numc; i++)
-	    {        // Process bytes received
-
+	    {
 	        // read the next byte
 	        c = (char)port->read();
-	        hal.console->printf("%c",c);
+
+	        // Start looking for the JSON string
+	        if(c == '{')
+	        {
+	        	// I've found the header
+	        	msg[j] = c;
+	        	found_header = true;
+	        }
+	        else
+	        {
+		        if(found_header)
+		        {
+		        	j += 1;
+		        	if(c != '}')
+		        		msg[j] = c;
+		        	else
+		        	{
+		        		msg[j] = '}';
+		        		msg[j+1] = '\0';
+		        		hal.console->printf("%s",msg);
+		        		parsed = true;
+		        	}
+		        }
+	        }
 
 	    }
+
 	    return parsed;
 }
-
-
-
-//	// hal.console->printf("TEST Message AP_VisionPose_Jetson\n");
-//
-//	char buf[250];
-//	unsigned int i = 0;
-//
-//    bool parsed = false;
-//
-//    char c = '0';
-//
-//    // port->available() > 0 &&
-//	while(c != '#' && i < sizeof(buf)-1)
-//	{
-//		  hal.console->printf("%c",c);
-//	      c = port->read();
-//	      buf[i++] = c;
-//	}
-//
-//	// buf[sizeof(buf)-1] = '\0';
-//	buf[249] = '\0';
-//
-//	//hal.console->printf("Received Message: %s\n",buf);
-//
-//    return parsed;
-
-// }
